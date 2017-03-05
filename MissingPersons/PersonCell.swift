140feddc48b49dacc164f9d08b6f24c3e5f90b32
mshayanh13 <mohammadhemani@gmail.com>
2017-03-05 09:48:51 -0600

    UI and CollectionView

---
+++ MissingPersons/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  MissingPersons
+//
+//  Created by Mohammad Hemani on 3/5/17.
+//  Copyright © 2017 Mohammad Hemani. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    
+    
+    
+}
--- MissingPersons/ViewController.swift
+++ MissingPersons/ViewController.swift
@@ -8,17 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var selectedImage: UIImageView!
+    
+    let baseURL = "http://localhost:6069/img"
+    let missingPeople = [ "person1.jpg", "person2.jpg", "person3.jpg", "person4.jpg", "person5.jpg" , "person6.jpg"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func checkForMatch(_ sender: UIButton) {
+        
+        
+        
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return missingPeople.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PersonCell", for: indexPath) as! PersonCell
+        
+        return cell
+        
+    }
+    
+    
 
 
 }
