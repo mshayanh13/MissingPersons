492b908c982639a2d4e2b945db7752a174140d61
mshayanh13 <mohammadhemani@gmail.com>
2017-03-05 11:16:51 -0600

    Microsoft Cognitive iOS SDK

--- MissingPersons/ViewController.swift
+++ MissingPersons/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import ProjectOxfordFace
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
